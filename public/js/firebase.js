be2bb476c7f334211270ef382e0574cdd7d1bab7
Sofia Neo <sofineo@gmail.com>
2023-12-01 14:22:29 -0800

    JS file with Firebase configs

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+/* Firebase Configurations */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkKXZ2XsTNf38mlG9YmOlmY2vfoBpyWdo",
+  authDomain: "cctb-ist103-assigment03.firebaseapp.com",
+  projectId: "cctb-ist103-assigment03",
+  storageBucket: "cctb-ist103-assigment03.appspot.com",
+  messagingSenderId: "398139109896",
+  appId: "1:398139109896:web:dbe919049211b52d8957bd"
+};
+
+firebase.initializeApp(firebaseConfig);
